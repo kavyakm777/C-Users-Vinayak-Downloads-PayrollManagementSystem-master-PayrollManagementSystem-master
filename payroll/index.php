6ca6358d0ca5542df97386a076bfeeb6ecc8aa74
albatross <76097246+kavyakm777@users.noreply.github.com>
2021-01-15 12:57:27 +0530

    Add files via upload
    Added files

---
+++ payroll/index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+       <head>
+	        <meta charset="utf-8">
+	        <title>Welcome to homepage</title>
+
+
+			<link rel="stylesheet" href="style.css">
+	   </head>
+	   <body>
+	   
+	        <div class="loginbox">
+			    <h2>LOG IN</h2>
+			    <form method="post" action="newfile.php">
+				     <p>User Name</p>
+					 <input type="text" name="username" placeholder="">
+					 <p>Password</p>
+					 <input type="password" name="password" placeholder="">
+					 <input type="submit" name="submit" value="Submit">
+				</form>
+			</div>
+	   </body>
+</html>
